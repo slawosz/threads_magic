2502ebc67f573c057a6681e9c7cc48eab04b84a9
Sławosz <slawosz@gmail.com>
2013-07-24 20:06:11 +0200

    adjust example

--- example/runner/basic.rb
+++ example/runner/basic.rb
@@ -1,9 +1,9 @@
 require_relative '../../lib/runner'
 
 Runner.loop runs: 2 do |r|
-  puts :foo
+  r.run { puts :foo }
   r.loop runs: 3 do
-    puts "  bar"
-    puts "  baz"
+    r.run { puts "  bar" }
+    r.run { puts "  baz" }
   end
 end
