c401843b0d22522ed47635f36bea7e5df5dbd074
Sławosz <slawosz@gmail.com>
2013-07-24 20:05:42 +0200

    better runner structure

--- lib/runner.rb
+++ lib/runner.rb
@@ -1,12 +1,26 @@
 class Runner
 
   def self.loop(opts = { runs: 1 }, &block)
-    new.loop(opts, &block)
+    runner = new
+    runner.loop(opts, &block)
+    runner.run_all
+  end
+
+  def initialize
+    @collected_runs = []
   end
 
   def loop(opts = { runs: 1 }, &block)
     opts[:runs].times do
       block.call(self)
     end
+  end
+
+  def run(&block)
+    @collected_runs << block
+  end
+
+  def run_all
+    @collected_runs.each &:call
   end
 end
