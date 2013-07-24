588d84324f81bcd841090ce06578704da7555ba2
Sławosz <slawosz@gmail.com>
2013-07-24 14:30:09 +0200

    better examples

---
+++ example/multiple_callbacks.rb
@@ -0,0 +1,19 @@
+require_relative '../lib/ticker'
+
+ticker = Ticker.new(1.5)
+
+ticker.on_tick do
+  puts Time.now.strftime("tick1: %T.%L")
+end
+
+ticker.on_tick do
+  puts Time.now.strftime("tick2: %T.%L")
+end
+
+puts Time.now.strftime("%T.%L")
+ticker.start
+sleep 6.1
+puts Time.now.strftime("end: %T.%L")
+ticker.stop
+puts 'No more ticking'
+sleep 5
